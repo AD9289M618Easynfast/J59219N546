df182685073f9a04bd5a45de3635bf967e9b07cc
zBaoAnhLe <zBaoAnhLe@gmail.com>
2014-06-20 18:04:26 +0700

    làm trang

--- page-services.php
+++ page-services.php
@@ -1,23 +1,60 @@
 <?php
-include_once 'seoSupport.php';
-$underDevelopment = true;
+include_once "seoSupport.php";
+$underDevelopment = false;
+
+global $imagePath;
+global $imageIconPath;
+global $imageFbPath;
+global $bannerImage;
 
 $bannerImage = "banner-services.png";
-
-$titleSeo = "";
-$keywordSeo = "easynfast services, design website, convert psd to website, create wordpress theme";
-$descriptionSeo = "EasynFast Services, We're a group of elites website developer and we are here to convert your idea become reality.";
-$imageSeo = "";
+$titleSeo = "EasynFast's Services";
+$keywordSeo = "easynfast services";
+$descriptionSeo = "EasynFast Services, We're a group of elites website developer and we are here to turn your idea become reality.";
+$imageSeo = $imageFbPath . "fb-services.png";
 
 get_header();
 ?>
 <div id="main-content" <?php
-     if ($underDevelopment) {
-         echo 'class="under-development"';
-     }
-     ?>>
+if ($underDevelopment) {
+    echo 'class="under-development"';
+}
+?>>
          <?php if (!$underDevelopment) : ?>
-        Do your things here
+        <div class="services-wrapper">
+            <div class="services-content">
+                Easynfast's have many different services. Even so, our focus are website development! Contact us now to get your work done professionally!
+            </div>
+
+            <div class="services-list-wrapper">
+                <ul class="services-list">
+                    <li>
+                        <div class="main-info fleft">
+
+                        </div>
+                        <div class="sub-info fright">
+
+                        </div>
+                    </li>
+                    <li>
+                        <div class="main-info fright">
+
+                        </div>
+                        <div class="sub-info fleft">
+
+                        </div>
+                    </li>
+                    <li>
+                        <div class="main-info fleft">
+
+                        </div>
+                        <div class="sub-info fright">
+
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     <?php endif; ?>
 </div>
 <?php
--- template/template.php
+++ template/template.php
@@ -1,17 +1,45 @@
 <?php
-include_once 'seoSupport.php';
+include_once "seoSupport.php";
 $underDevelopment = false;
+/**
+ * *********************************************
+ * Methods Summary: Global Variables
+ * *********************************************
+ */
+global $imagePath;
+global $imageIconPath;
+global $imageFbPath;
+global $bannerImage;
 
+/**
+ * *********************************************
+ * Methods Summary: SEO Optimize Variables
+ * *********************************************
+ */
+//Banner Images
 $bannerImage = "";
 
+//SEO Titles. if "" > default title
 $titleSeo = "";
+
+//SEO Keywords
 $keywordSeo = "";
+
+//SEO Description
 $descriptionSeo = "";
+
+//SEO Facebook Images.
 $imageSeo = "";
 
 get_header();
 ?>
 <div id="main-content" <?php
+     /**
+      * *********************************************
+      * Methods Summary: If is under development Content 
+      * won't show.
+      * *********************************************
+      */
      if ($underDevelopment) {
          echo 'class="under-development"';
      }
