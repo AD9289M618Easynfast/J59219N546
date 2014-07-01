f3289eb76c94e446e60e98a21c86d5beb9f0f3df
zBaoAnhLe <zBaoAnhLe@gmail.com>
2014-07-02 00:25:14 +0700

    thêm trang

--- js/main.js
+++ js/main.js
@@ -202,3 +202,22 @@ $(document).ready(function() {
         $('.overlay').fadeOut(200);
     });
 });
+/**
+ * *********************************************
+ * Methods Summary: Methods for Smoothscrolling
+ * *********************************************
+ */
+$(function() {
+    $('.smoothscroll').bind('click.smoothscroll', function(e) {
+        e.preventDefault();
+
+        var target = this.hash,
+                $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 0
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
